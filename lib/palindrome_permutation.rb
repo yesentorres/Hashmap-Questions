b7706944375b911ef9cbce9216a5a76696183f1b
Yesenia <yesentorres@gmail.com>
2020-03-24 20:50:40 -0700

    permutations method passes all test

--- lib/palindrome_permutation.rb
+++ lib/palindrome_permutation.rb
@@ -1,4 +1,4 @@
 
 def palindrome_permutation?(string)
-  raise NotImplementedError, "palindrome_permutation? not implemented"
+  
 end
--- lib/permutations.rb
+++ lib/permutations.rb
@@ -1,4 +1,27 @@
 
 def permutations?(string1, string2)
-  raise NotImplementedError, "permutations? not implemented"
+
+  # if strings are not same length, can assume they aern't permutations of one another
+  if string1.length > string2.length || string2.length > string1.length
+    return false
+  end 
+
+  # convert strings to arrays
+  string1_array = string1.split("")
+  string2_array = string2.split("")
+
+  # store letters of string1 in a hash
+  hash = {}
+  string1_array.each do |char|
+    hash[char] = true
+  end
+
+  # check if there's a letter that appears in string2 that does not appear in string1
+  string2_array.each do |char|
+    if hash.has_key?(char) != true  
+      return false
+    end
+  end 
+
+  return true 
 end
